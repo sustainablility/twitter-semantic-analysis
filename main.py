beda71ff5c60345977022f430cc23402666a0d8a
Matthew Kobilas <mattkobi789@gmail.com>
2019-11-04 21:32:46 -0500

    Working version of tool
    Will build sentiment for tweets in a json based on NaiveBayes model and NLTK twitter_sample corpus

--- main.py
+++ main.py
@@ -8,14 +8,11 @@ import string
 import re
 import os
 from os import path
-from PIL import Image
-from bs4 import BeautifulSoup
 from time import sleep
 from collections import Counter
 from nltk.tokenize import TweetTokenizer
 from nltk.classify import NaiveBayesClassifier
 from nltk.corpus import twitter_samples
-from textblob import TextBlob
 from random import shuffle
 
 # abstraction:
@@ -23,6 +20,24 @@ from random import shuffle
 # https://kite.com/python/docs/nltk.TweetTokenizer
 # https://blog.chapagain.com.np/python-nltk-twitter-sentiment-analysis-natural-language-processing-nlp/
 # https://www.kaggle.com/nicewinter/gop-twitter-sentiment-analysis-in-python
+# https://stackoverflow.com/questions/20827741/nltk-naivebayesclassifier-training-for-sentiment-analysis
+
+def stemTweets(tweetList):
+    sno = nltk.stem.SnowballStemmer("english")
+    retTweetList = []
+    for words in tweetList:
+        stemmed_words = [sno.stem(word) for word in words]
+        retTweetList.append(stemmed_words)
+    return retTweetList
+
+def tokenizeTweets(tweetList):
+    useless_ones = nltk.corpus.stopwords.words("english") + list(string.punctuation)
+    tokenizer = TweetTokenizer(preserve_case=False, strip_handles=False, reduce_len=False)
+    retTweetList = []
+    for tweet in tweetList:
+        wordlist = [word for word in tokenizer.tokenize(tweet) if word not in useless_ones]
+        retTweetList.append(wordlist)
+    return retTweetList
 
 def cleanTweets(tweet):
     # make lowercase
@@ -44,6 +59,7 @@ def cleanTweets(tweet):
     tweet = re.sub(r"https?:\/\/.*[\r\n]*", "", tweet)
     # remove hashtags
     tweet = re.sub(r"#", "", tweet)
+    return tweet
 
 # simplest model for analyzing text is to think of it as an unordered list of words
 # known as bag-of-words model
@@ -86,64 +102,60 @@ def main(twtInfo: object):
         ...
     ]
     """
-    data = pd.read_json(twtInfo, orient="records")
+    data = pd.read_json(twtInfo, orient="records", lines=True)
+    nltk.download("twitter_samples")
     pos_tweets = twitter_samples.strings("positive_tweets.json")
     neg_tweets = twitter_samples.strings("negative_tweets.json")
     # sort values by id, data in json that is in repo is already sorted by id
-    data.sort_values(by="3")
-    tweets = data["text"]
-    labels = data["sentiment"]
-    clean_tweets = [cleanTweets(tweet) for tweet in tweets]
+    #data.sort_values(by="3")
+    data_tweets = data["text"]
+    clean_data_tweets = [cleanTweets(tweet) for tweet in data_tweets]
+    clean_pos_tweets = [cleanTweets(tweet) for tweet in pos_tweets]
+    clean_neg_tweets = [cleanTweets(tweet) for tweet in neg_tweets]
     # downloads corpus of stopwords (i.e. "the", "did", "?")
     # TODO: check if nltk.stopwords is already downloaded and if it is, then skip
     nltk.download("stopwords")
-    # filtering out stop words and punctuation
-    # may want to add additional filtering criteria depending upon category of tweets
-    # i.e. GOP debate dataset used this filter criteria:
-    #useless_ones = nltk.corpus.stopwords.words("english") + list(string.punctuation) + ['``', "''",'gop','debate','gopdeb','gopdebate','gopdebates','fox','news','foxnew','foxnews', 'amp']
-    # default criteria below
-    useless_ones = nltk.corpus.stopwords.words("english") + list(string.punctuation)
     # downloads work tokenizer trained on English
     # TODO: check if nltk.punkt is already downloaded and if it is, then skip
     nltk.download("punkt")
     # tokenize and clean up the whole set of clean tweet texts
     # tc_tweets = tokenized & cleaned tweets
-    tc_tweets = []
-    tokenizer = TweetTokenizer(preserve_case=False, strip_handles=False, reduce_len=False)
-    for tweet in clean_tweets:
-        # creates a list of words for each tweet
-        wordlist = [word for word in tokenizer.tokenize(tweet) if word not in useless_ones]
-        tc_tweets.append(wordlist)
+    data_tc_tweets = tokenizeTweets(clean_data_tweets)
+    pos_tc_tweets = tokenizeTweets(clean_pos_tweets)
+    neg_tc_tweets = tokenizeTweets(clean_neg_tweets)
     # apply stemming algorithm to tweets
     # stemming normalizes text i.e. "waited", "waits", "waiting" -> "wait"
     # this cleans the data and makes it easier for the ML algorithm to read it
-    sno = nltk.stem.SnowballStemmer("english")
-    tc_tweets_stemmed = []
-    for words in tc_tweets:
-        stemmed_words = [sno.stem(word) for word in words]
-        tc_tweets_stemmed.append(stemmed_words)
-    tc_tweets[:] = tc_tweets_stemmed
+    data_tcs_tweets = stemTweets(data_tc_tweets)
+    pos_tcs_tweets = stemTweets(pos_tc_tweets)
+    neg_tcs_tweets = stemTweets(neg_tc_tweets)
     # pairs each tweet's cleaned text with its sentiment label
-    text_label_pair_list = list(zip(tc_tweets, labels))
+    pos_label_pair_list = ((tweet, "pos") for tweet in pos_tcs_tweets)
+    neg_label_pair_list = ((tweet, "neg") for tweet in neg_tcs_tweets)
+    # TODO: possible bias location, we are only separating into positive/negative sentiment and not neutral
     # remove all neutral tweets since we are only interested in positive/negative ones
-    text_label_pair_list[:] = [tuple for tuple in text_label_pair_list if tuple[1] != "Neutral"]
+    #text_label_pair_list[:] = [tuple for tuple in text_label_pair_list if tuple[1] != "Neutral"]
     # split into train and test set, 90% for training set, 10% for testing set
-    train, test = train_test_split(text_label_pair_list, test_size = 0.1, random_state=7)
+    #train, test = train_test_split(text_label_pair_list, test_size = 0.1, random_state=7)
     # define bag-of-words model and features
-    train_bow = [(buildBowFeatures(tuple[0]), tuple[1]) for tuple in train]
-    test_bow = [(buildBowFeatures(tuple[0]), tuple[1]) for tuple in test]
+    pos_bow = [(buildBowFeatures(tuple[0]), tuple[1]) for tuple in pos_label_pair_list]
+    neg_bow = [(buildBowFeatures(tuple[0]), tuple[1]) for tuple in neg_label_pair_list]
+    data_bow = [buildBowFeatures(text) for text in data_tcs_tweets]
     # one of the simplest supervised ML classifiers is the Naive Bayes Classifier
     # TODO: potential new tool would involve different ML classifier
     # it can be trained on 90% of the data to learn what words are associated with pos/neg comments
+    train_bow = pos_bow + neg_bow
+    shuffle(train_bow)
     sentiment_classifier = NaiveBayesClassifier.train(train_bow)
     # we can check after training what the accuracy is on the training set
     # i.e. the same data we used for training, this should be a high number since algo already saw the data
     #nltk.classify.util.accuracy(sentiment_classifier, train_bow)*100
     # accuracy on the testing set
     #nltk.classify.util.accuracy(sentiment_classifier, test_bow)*100
-    test_comment_dicts, test_labels = list(zip(*test_bow))
-    preds = [sentiment_classifier.classify(comment_dict) for comment_dict in test_comment_dicts]
-    pred_vs_observ = pd.DataFrame(np.array([test_labels, preds]).T, columns=["observation", "prediction"])
-    pred_vs_observ.transpose()
+    preds = [sentiment_classifier.classify(comment_dict) for comment_dict in data_bow]
     # TODO: figure out what to return
     #return pd.Series(rxt_params).to_json(orient="records")
+    #dfPreds = pd.DataFrame(preds)
+    #ret = pd.concat([data, dfPreds], axis=1)
+    return pd.Series(preds).to_json(orient="records")
+    
