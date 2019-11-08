f3321fed94cde84d1bb47786b2e4bd7617cba57b
Matthew Kobilas <mattkobi789@gmail.com>
2019-11-07 21:47:05 -0500

    main_clean.py working with json input and output

--- main_clean.py
+++ main_clean.py
@@ -1,3 +1,20 @@
+from __future__ import division
+import json
+import pandas as pd
+import numpy as np
+import requests
+import nltk
+import string
+import re
+import os
+from os import path
+from time import sleep
+from collections import Counter
+from nltk.tokenize import TweetTokenizer
+from nltk.classify import NaiveBayesClassifier
+from nltk.corpus import twitter_samples
+from random import shuffle
+
 def cleanTweet(tweet):
     # make lowercase
     tweet = tweet.lower()
@@ -50,5 +67,15 @@ def cleanTweet(tweet):
 def main(twtInfo:object):
     data = pd.read_json(twtInfo, orient="records", lines=True)
     data_tweets = data["text"]
+    data_id = data["id"]
     clean_data_tweets = [cleanTweet(tweet) for tweet in data_tweets]
-    return pd.Series(clean_data_tweets).to_json(orient="records")
+    ret = []
+    for i in range(len(data_id)):
+        ret.append({})
+        ret[i]["text"] = clean_data_tweets[i]
+        ret[i]["id"] = data_id[i]
+    return pd.Series(ret).to_json()
+
+dat = main("C:/Users/Matt/Documents/GitHub/twitter-sentiment-analysis/test.json")
+with open("test_clean_out.json", "w+") as out:
+    out.write(dat)
--- main_tokenize.py
+++ main_tokenize.py
@@ -1,3 +1,20 @@
+from __future__ import division
+import json
+import pandas as pd
+import numpy as np
+import requests
+import nltk
+import string
+import re
+import os
+from os import path
+from time import sleep
+from collections import Counter
+from nltk.tokenize import TweetTokenizer
+from nltk.classify import NaiveBayesClassifier
+from nltk.corpus import twitter_samples
+from random import shuffle
+
 def tokenizeTweets(tweetList):
     useless_ones = nltk.corpus.stopwords.words("english") + list(string.punctuation)
     tokenizer = TweetTokenizer(preserve_case=False, strip_handles=False, reduce_len=False)
@@ -22,3 +39,7 @@ def main(twtInfo:object):
     data_tc_tweets = tokenizeTweets(clean_data_tweets)
     data_tcs_tweets = stemTweets(data_tc_tweets)
     return pd.Series(data_tcs_tweets).to_json(orient="records")
+
+dat = main("C:/Users/Matt/Documents/GitHub/twitter-sentiment-analysis/test_clean_out.json")
+with open("test_token_out.json", "w+") as out:
+    out.write(dat)
